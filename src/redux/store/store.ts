b0da4202e54fcabf4cc2016d36c990bed024d1e5
allan <ajbtolentino@gmail.com>
2022-03-28 02:31:49 +0800

    Fixed redux error

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,11 +1,11 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
-import { sampleReducer } from "../reducers/SampleReducer";
-import { todoReducer } from "../reducers/TodoReducer";
+import { SampleReducer } from "../reducers/SampleReducer";
+import { TodoReducer } from "../reducers/TodoReducer";
 import { RootState } from "./RootState";
 
 const reducers = combineReducers<RootState>({
-    sample: sampleReducer,
-    todo: todoReducer
+    sample: SampleReducer,
+    todo: TodoReducer
 });
 
 export const Store = createStore(reducers, applyMiddleware());
