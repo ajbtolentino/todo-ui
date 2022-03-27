68a6a42c621469ab2a71be451c91ffe378460be5
allan <ajbtolentino@gmail.com>
2022-03-28 00:21:30 +0800

    Updated dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,13 +1,27 @@
+import { Card, CardContent, Grid, Typography } from "@mui/material";
 import { useTodo } from "../hooks/useTodo";
 
 export const Dashboard = () => {
     const {todos: tasks} = useTodo();
 
     return (
-        <div>
-            Total:{tasks.filter(_ => !_.completed).length}
-            Pending: {tasks.filter(_ => !_.completed).length}
-            Completed: {tasks.filter(_ => _.completed).length}
-        </div>
+        <Card style={{backgroundColor: "lightgoldenrodyellow"}}>
+            <CardContent>
+                <Grid container direction={"row"} justifyContent={"space-around"}>
+                    <Grid item xs={4}>
+                        <Typography>Total</Typography>
+                        {tasks.filter(_ => !_.completed).length}
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Typography>Pending</Typography>
+                        {tasks.filter(_ => !_.completed).length}
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Typography>Completed</Typography>
+                        {tasks.filter(_ => _.completed).length}
+                    </Grid>
+                </Grid>
+            </CardContent>
+        </Card>
     );
 }
