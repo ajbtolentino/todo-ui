f54792e48058a4f1e2b390dde552fa5b76153b19
Allan Tolentino <ajbtolentino@gmail.com>
2022-04-02 01:22:22 +0800

    Added filter action

--- src/components/Todo/List.tsx
+++ src/components/Todo/List.tsx
@@ -1,23 +1,39 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useTodo } from "../../hooks/useTodo";
+import { ITodo } from "../../models/ITodo";
 import { Details } from "./Details";
 
 export const List = () => {
-    const { todos } = useTodo();
+    const { todos, filter } = useTodo();
+    const [currentTodos, setCurrentTodos] = useState<ITodo[]>(todos);
+    
+    useEffect(() => {
+        switch(filter) {
+            case "COMPLETED":
+                setCurrentTodos([...todos.filter(todo => todo.completed)]);
+                break;
+            case "PENDING":
+                setCurrentTodos([...todos.filter(todo => !todo.completed)]);
+                break;
+            default:
+                setCurrentTodos(todos);
+        }
+
+    }, [todos, filter]);
 
     return(
     <>
         {
             /** Load all tasks if there are any */
-            todos.length > 0 && 
-            todos.map(todo => 
+            currentTodos.length > 0 && 
+            currentTodos.map(todo => 
                 <div key={todo.id} className="todo">
                     <Details {...todo} />
                 </div>)
         }
         {
             /** Show an empty message */
-            !todos.length && <h2 style={{textAlign: "center"}}>You don't have any tasks!</h2>
+            !currentTodos.length && <h2 style={{textAlign: "center"}}>You don't have any tasks!</h2>
         }
     </>
     );
